9c1151c8eaf8bd83561819090105029907047a86
Max Cutler <max@maxcutler.com>
2014-06-05 21:05:39 -0700

    Create console app to test the API library.

---
+++ ApiConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ApiConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter site URL:");
+			var url = Console.ReadLine();
+
+			Console.WriteLine("Searching for API at {0}", url);
+
+			Console.WriteLine("Press any key to close.");
+			Console.Read();
+		}
+	}
+}
