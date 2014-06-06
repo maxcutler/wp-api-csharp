a69f8b950233d98fa07acf5137b51beb665528cb
Max Cutler <max@maxcutler.com>
2014-06-05 21:31:45 -0700

    Initiate API discovery through the console app.

--- ApiConsole/Program.cs
+++ ApiConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using PortableWordPressApi;
 
 namespace ApiConsole
 {
@@ -6,13 +9,24 @@ namespace ApiConsole
 	{
 		static void Main(string[] args)
 		{
+			Task.WhenAll(AsyncMain());
+
+			Console.WriteLine("Press any key to close.");
+			Console.Read();
+		}
+
+		private static async Task AsyncMain()
+		{
 			Console.WriteLine("Enter site URL:");
 			var url = Console.ReadLine();
 
 			Console.WriteLine("Searching for API at {0}", url);
 
-			Console.WriteLine("Press any key to close.");
-			Console.Read();
+			var httpClient = new HttpClient();
+			var discovery = new WordPressApiDiscovery(new Uri(url, UriKind.Absolute), httpClient);
+			var api = await discovery.DiscoverApiForSite();
+
+			Console.WriteLine("Site's API endpoint: {0}", api.ApiRootUri);
 		}
 	}
 }
