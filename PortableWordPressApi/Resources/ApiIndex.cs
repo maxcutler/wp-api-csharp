c10883570d75d3c45c9a0772a3b473ea89fbe24d
Max Cutler <max@maxcutler.com>
2014-06-07 09:52:18 -0400

    Start stubbing parsing of index document.

---
+++ PortableWordPressApi/Resources/ApiIndex.cs
@@ -0,0 +1,13 @@
+﻿namespace PortableWordPressApi.Resources
+{
+	public class ApiIndex
+	{
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public string URL { get; set; }
+
+		public Meta Meta { get; set; }
+	}
+}
---
+++ PortableWordPressApi/Resources/Meta.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PortableWordPressApi.Resources
+{
+	public class Meta
+	{
+		public Dictionary<string, string> Links { get; set; }
+	}
+}
