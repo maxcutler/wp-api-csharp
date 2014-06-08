62229826f203be89367721ac5250591d12026fcf
Max Cutler <max@maxcutler.com>
2014-06-08 13:21:53 -0400

    Add User resource.

---
+++ PortableWordPressApi/Resources/User.cs
@@ -0,0 +1,17 @@
+﻿namespace PortableWordPressApi.Resources
+{
+	public class User
+	{
+		public string ID { get; set; }
+
+		public string Name { get; set; }
+
+		public string Slug { get; set; }
+
+		public string Url { get; set; }
+
+		public string Avatar { get; set; }
+
+		public Meta Meta { get; set; }
+	}
+}
