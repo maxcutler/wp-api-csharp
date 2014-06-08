208eacf5b736e324c12d5a81c90edc60f8fc563d
Max Cutler <max@maxcutler.com>
2014-06-08 13:24:14 -0400

    Add Post Meta resource.

---
+++ PortableWordPressApi/Resources/PostMeta.cs
@@ -0,0 +1,11 @@
+﻿namespace PortableWordPressApi.Resources
+{
+	public class PostMeta
+	{
+		public string Id { get; set; }
+
+		public string Key { get; set; }
+
+		public string Value { get; set; }
+	}
+}
