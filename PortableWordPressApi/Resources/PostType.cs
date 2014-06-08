9b8c8b0112faabc70e8991a1e7ed61b46c608740
Max Cutler <max@maxcutler.com>
2014-06-08 13:09:24 -0400

    Add Post Type resource.

---
+++ PortableWordPressApi/Resources/PostType.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace PortableWordPressApi.Resources
+{
+	public class PostType
+	{
+		public string Name { get; set; }
+
+		public string Slug { get; set; }
+
+		public string Description { get; set; }
+
+		public bool Hierarchical { get; set; }
+
+		public bool Queryable { get; set; }
+
+		public bool Searchable { get; set; }
+
+		public Dictionary<string, string> Labels { get; set; } 
+
+		public Meta Meta { get; set; }
+	}
+}
