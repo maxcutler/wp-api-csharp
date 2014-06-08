11faf981abd9ac86ee374f903eec9aa1ed5924b1
Max Cutler <max@maxcutler.com>
2014-06-08 14:38:51 -0400

    Add Post resource.

--- PortableWordPressApi/Resources/Post.cs
+++ PortableWordPressApi/Resources/Post.cs
@@ -1,9 +1,59 @@
-﻿namespace PortableWordPressApi.Resources
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PortableWordPressApi.Resources
 {
 	public class Post
 	{
 		public string ID { get; set; }
 
 		public string Title { get; set; }
+
+		[JsonProperty("date_gmt")]
+		public DateTime DateGmt { get; set; }
+
+		[JsonProperty("modified_gmt")]
+		public DateTime ModifiedGmt { get; set; }
+
+		public string Status { get; set; }
+
+		public string Type { get; set; }
+
+		public string Name { get; set; }
+
+		public User Author { get; set; }
+
+		public string Password { get; set; }
+
+		public string Content { get; set; }
+
+		public string Excerpt { get; set; }
+
+		public string Parent { get; set; }
+
+		public string Link { get; set; }
+
+		public string Guid { get; set; }
+
+		[JsonProperty("menu_order")]
+		public int MenuOrder { get; set; }
+
+		[JsonProperty("comment_status")]
+		public string CommentStatus { get; set; }
+
+		[JsonProperty("ping_status")]
+		public string PingStatus { get; set; }
+
+		public bool Sticky { get; set; }
+
+		// thumbnail
+
+		[JsonProperty("post_format")]
+		public string PostFormat { get; set; }
+
+		[JsonProperty("post_meta")]
+		public PostMeta PostMeta { get; set; }
+
+		public Meta Meta { get; set; }
 	}
 }
