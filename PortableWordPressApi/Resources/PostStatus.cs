dfd119284621d9a45232b29adb95c2fc8fa56a5c
Max Cutler <max@maxcutler.com>
2014-06-08 13:15:47 -0400

    Add Post Status resource.

---
+++ PortableWordPressApi/Resources/PostStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace PortableWordPressApi.Resources
+{
+	public class PostStatus
+	{
+		public string Name { get; set; }
+
+		public string Slug { get; set; }
+
+		public bool Public { get; set; }
+
+		public bool Protected { get; set; }
+
+		public bool Private { get; set; }
+
+		public bool Queryable { get; set; }
+
+		public bool ShowInList { get; set; }
+
+		public Meta Meta { get; set; }
+	}
+}
